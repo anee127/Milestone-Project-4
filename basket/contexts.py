bf563e8d6f9f7c8c16cb817d737c3cd42451843d
Anee <aneesa.khan97@outlook.com>
2022-02-11 18:15:52 +0000

    created add to basket functionality and button

--- basket/contexts.py
+++ basket/contexts.py
@@ -6,32 +6,13 @@ def basket_contents(request):
     basket_items = []
     total = 0
     product_count = 0
-    basket = request.session.get('basket', {})
 
-      for item_id, item_data in basket.items():
-        if isinstance(item_data, int):
-            product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
-            product_count += item_data
-            basket_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'product': product,
-            })
-        else:
-            product = get_object_or_404(Product, pk=item_id)
-            for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price
-                product_count += quantity
-                basket_items.append({
-                    'item_id': item_id,
-                    'quantity': quantity,
-                    'product': product,
-                    'size': size,
-                })
-
-    delivery = 0
-    
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
     
     grand_total = delivery + total
     
@@ -40,6 +21,8 @@ def basket_contents(request):
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
 
