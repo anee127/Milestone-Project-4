737963add8afdcf8940d52c2ddac0f848ae9d5b3
Anee <aneesa.khan97@outlook.com>
2022-02-21 16:40:28 +0000

    added checkout app and models

--- basket/tests.py
+++ basket/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class BasketViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_basket_get(self):
+        url = reverse("view_basket")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Shopping Basket")
+
--- basket/views.py
+++ basket/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect,\
+     reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
 
 # Create your views here.
 
+
 def view_basket(request):
     """ A view that renders the basket contents page """
 
     return render(request, 'basket/basket.html')
+
 
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the shopping basket """
@@ -20,30 +23,34 @@ def add_to_basket(request, item_id):
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
-        messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
+        messages.success(
+            request, f'Updated {product.name} quantity to {basket[item_id]}')
     else:
         basket[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your basket') 
+        messages.success(request, f'Added {product.name} to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
 
 def adjust_basket(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
-    basket = request.session.get('basket', {})    
+    basket = request.session.get('basket', {})
 
     if quantity > 0:
-            basket[item_id] = quantity
-            messages.success(request, (f'Updated {product.name} quantity to {basket[item_id]}'))
+        basket[item_id] = quantity
+        messages.success(
+            request, (f'Updated {product.name} quantity to {basket[item_id]}'))
     else:
         basket.pop(item_id)
-        messages.success(request,(f'Removed {product.name} from your basket'))
+        messages.success(request, (f'Removed {product.name} from your basket'))
 
     request.session['basket'] = basket
-    return redirect(reverse('view_basket'))      
+    return redirect(reverse('view_basket'))
+
 
 def remove_from_basket(request, item_id):
     """ Remove the item from the shopping cart """
@@ -52,11 +59,12 @@ def remove_from_basket(request, item_id):
         product = get_object_or_404(Product, pk=item_id)
         basket = request.session.get('basket', {})
         basket.pop(item_id)
-        messages.success(request, f'Removed {product.name.title()} from your basket')
+        messages.success(
+            request, f'Removed {product.name.title()} from your basket')
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
 
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
-        return HttpResponse(status=500)                    
+        return HttpResponse(status=500)
