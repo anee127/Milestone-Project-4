6068cc59ec970617e1eac84a1ac39aa498c0882f
Anee <aneesa.khan97@outlook.com>
2022-02-25 02:10:53 +0000

    removed scret key and set debug

--- vanilla_musk/settings.py
+++ vanilla_musk/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oi=cokt(ma90ln14$1o7z!1r$nff5(lm0=zxd+^wqkvqrl3dm@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ckz8780-boutique-ado.herokuapp.com', 'localhost']
 
