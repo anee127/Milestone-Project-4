12055cbf90f8c4aaeed323547c742b1ed4d381d9
Anee <aneesa.khan97@outlook.com>
2022-02-25 18:21:41 +0000

    057 - deployment part 3, changes to setting

--- vanilla_musk/settings.py
+++ vanilla_musk/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ckz8780-boutique-ado.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['aneesa127-vanilla-musk.herokuapp.com', 'localhost']
 
 
 # Application definition
