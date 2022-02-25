c0db33417368a6ee2d568db108f1be712b11e5d5
Anee <aneesa.khan97@outlook.com>
2022-02-25 02:00:21 +0000

    055 - deployment part 2

--- vanilla_musk/settings.py
+++ vanilla_musk/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-oi=cokt(ma90ln14$1o7z!1r$nff5(lm0=zxd+^wqkvqrl3dm@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ckz8780-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'vanilla_musk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
