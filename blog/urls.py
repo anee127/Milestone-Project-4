8781a76fd6387f7e7ef6decb79980e0325a145d6
Anee <aneesa.khan97@outlook.com>
2022-04-14 12:19:03 +0000

    changed blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.blog, name='blog'),
+    path('<slug:slug>/', views.blog_detail, name='blog_detail'),
+    path('add_blog', views.add_blog, name='add_blog'),
+    path('edit_blog/<slug:slug>/', views.edit_blog, name='edit_blog'),
+    path('delete_blog/<slug:slug>/', views.delete_blog, name='delete_blog'),
 ]
