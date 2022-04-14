6c99b09a2db933fa92287b1180e7c894cd993c57
Anee <aneesa.khan97@outlook.com>
2022-04-14 11:18:08 +0000

    made changes to blog d models, redid the migratios

--- blog/models.py
+++ blog/models.py
@@ -2,20 +2,21 @@ from django.db import models
 
 
 class Blog(models.Model):
-    blog_title = models.CharField(max_length=100, null=False, blank=False)
-    blog_author = models.TextField(max_length=100, null=False, blank=False)
-    blog_content = models.TextField(null=False, blank=False)
-    blog_date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.blog_title
+        return self.title
 
 
 class Comment(models.Model):
-    blog = models.ForeignKey(Blog, null=False, blank=False, on_delete=models.CASCADE)
-    username = models.CharField(max_length=255, null=True, blank=True,)
-    content = models.TextField(null=True, blank=True,)
+    blog = models.ForeignKey(Blog, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    statement = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.comment
+        return '%s - %s' % (self.blog.title, self.name)     
--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog, name='blog'),
-    path('add/', views.add_blog, name='add_blog'),
-    path('<int:blog_id>/', views.blog_detail, name='blog_detail'),
-    path('edit/<int:blog_id>', views.edit_blog, name='edit_blog'),
-    path('delete/<int:blog_id>', views.delete_blog, name='delete_blog'),
+    
 ]
