746b33f8f2e03b57c97cb5aad646109b35e3a6f8
Anee <aneesa.khan97@outlook.com>
2022-04-14 13:54:32 +0000

    added blog admin table

---
+++ blog/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Blog, Comment
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'content',
+        'date_added',
+    )
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'statement',
+        'date_added',
+    )
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment, CommentAdmin)    
